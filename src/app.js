62502a49d0cf6c1c0d7e318bf0f7eff3139d74f0
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2017-12-01 11:21:44 -0700

    add bootstrap

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import css from './app.scss';
-import React from 'react';
-import ReactDom from 'react-dom';
+import css from './scss/style.scss';
+// import React from 'react';
+// import ReactDom from 'react-dom';
 
-class App extends React.Component {
-    render(){
-        return(            
-            <h1>Hello World!!</h1> 
-        )
-    }
-}
+// class App extends React.Component {
+//     render(){
+//         return(            
+//             <h1>Hello World!!</h1> 
+//         )
+//     }
+// }
 
-ReactDom.render(<App />, document.getElementById('root'));
+// ReactDom.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 var path = require('path');
+var bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
 
 var isProd = process.env.NODE_ENV === 'production';
 var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 var cssProd = ExtractTextPlugin.extract({
     fallback: 'style-loader',
-    loader: ['css-loader', 'sass-loader'],
+    use: ['css-loader?-minimize', 'sass-loader?outputStyle=uncompressed'],
     publicPath: './'
 })
 var cssConfig = isProd ? cssProd : cssDev;
+var bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
     entry: {
-        app: './src/app.js'
+        app: './src/app.js',
+        boostrap: bootstrapConfig
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -22,22 +25,34 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.scss$/,
-            use: cssConfig
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        },
-        {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            use: [
-                'file-loader?name=img/[name].[ext]',
-                'image-webpack-loader'
-            ]
-        }
-    ]
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file-loader?name=img/[name].[ext]',
+                    'image-webpack-loader'
+                ]
+            },
+            {
+                test: /\.(woff2?|svg)$/,
+                use: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(ttf|eot)$/,
+                use: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+                use: 'imports-loader?jQuery=jquery'
+            }
+        ]
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
@@ -48,12 +63,12 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack Project',
+            title: 'Webpack Bootstrap 3 with React',
             favicon: './src/img/favicon.png',
             template: './src/index.html' // Load a custom template (lodash by default see the FAQ for details)
         }),
         new ExtractTextPlugin({
-            filename: 'style.css',
+            filename: './css/[name].css',
             disable: !isProd,
             allChunks: true
         }),
